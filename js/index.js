4a9cac69073a1c790c75face25c7b8e27a548b9c
jay.sjyun <jay.sjyun@gmail.com>
2026-06-14 13:54:13 +0900

    네비게이션 스크롤 개선, Swiper js 분리

--- js/index.js
+++ js/index.js
@@ -1,23 +1,25 @@
-const swiperAuto = new Swiper(".swiper_auto", {
-  loop: true,
+//Nav
+const menuContainer = document.querySelector(".nav__menu_list");
 
-  autoplay: {
-    delay: 1500,
-  },
+menuContainer.addEventListener("click", function (e) {
+  // 클릭된 요소가 nav__link 클래스를 가지고 있는지 확인
+  if (e.target.classList.contains("nav__link")) {
+    e.preventDefault(); // 히스토리 쌓이는 기본 이동 막기
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+    const targetId = e.target.getAttribute("href"); // "#section1" 같은 값 가져오기
+    const targetElement = document.querySelector(targetId);
 
-const swiper = new Swiper(".swiper", {
-  // loop: true,
+    if (targetElement) {
+      // fixed 메뉴바의 높이만큼 상단 여백을 주고 싶을 때 유용한 방식
+      const headerOffset = 40;
+      // 만약 고정 메뉴바 높이가 80px 라면 가려지지 않게 조절
+      const elementPosition = targetElement.getBoundingClientRect().top;
+      const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
+    }
+  }
 });
---
+++ js/swiper.js
@@ -0,0 +1,23 @@
+const swiperAuto = new Swiper(".swiper_auto", {
+  loop: true,
+
+  autoplay: {
+    delay: 1500,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiper = new Swiper(".swiper", {
+  // loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
